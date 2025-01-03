df0b982a61da4814a596e958a60f8668aabc06d4
Andrew Camara <andrew.camara@warwick.ac.uk>
2025-01-03 20:15:16 +0000

    Clean up TOP version removed some old code that had commented out.

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -6,7 +6,7 @@ let cpuScore = 0;
 function getComputerChoice() {
    //draw random number from 1-3, store in randNum
     let randNum = Math.floor((Math.random()*3))+1;
-    //If cpuChoice is 1, generate "rock"; if 2, generate "paper";
+    //If random number is 1, generate "rock"; if 2, generate "paper";
     //if 3, generate scissors; store result in cpuChoice
     if (randNum === 1) {
         return "rock";
@@ -71,34 +71,4 @@ function getPlayerChoice() {
     }
     const cpuChoice = getComputerChoice();
     const playerChoice = getPlayerChoice();
-    /*
-   function playRound(playerChoice, cpuChoice) {
-        let keepGoing = true;
-        while (keepGoing === true) {
-            //if same choice, keepgoing not updated, player 
-            // told to go again
-            if (playerChoice === cpuChoice) {
-                alert(`You selected ${playerChoice} and the computer selected ${cpuChoice}; go again!`);
-            } 
-            //if player wins, update score, inform player, and 
-            // finish the round by setting keepgoing to false
-            else if 
-            (playerChoice === "rock" && cpuChoice === "scissors"
-            || playerChoice === "paper" && cpuChoice === "rock"
-            || playerChoice === "scissors" && cpuChoice === "paper") { ++playerScore;
-                alert(`You selected ${playerChoice} and the computer selected ${cpuChoice}; you win the round! You are on ${playerScore} points and the computer is on ${cpuScore} points`);
-                keepGoing = false;
-            } 
-            //If cpu wins, increase cpuScore by 1, inform 
-            // player, and end the round by setting keepgoing 
-            // to false
-            else if 
-            (cpuChoice === "rock" && playerChoice === "scissors"
-                || cpuChoice === "paper" && playerChoice === "rock"
-                || cpuChoice === "scissors" && playerChoice === "paper") {
-                    ++cpuScore;
-                    alert(`You selected ${playerChoice} and the computer selected ${cpuChoice}; you lost the round! You are on ${playerScore} points and the computer is on ${cpuScore} points`);
-                    keepGoing = false;
-                }
-            }
-        } */
+ 
