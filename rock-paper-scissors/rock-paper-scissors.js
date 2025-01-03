fa2aa6e626b8389534b9d634257c7e9eb0d0cfb8
Andrew Camara <andrew.camara@warwick.ac.uk>
2025-01-03 15:37:31 +0000

    Create getChoice functions for cpu and player

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,17 +1,51 @@
-console.log("Hello World!")
-//Generate cpu choice
+console.log("Let the game begin!")
+//create variables: player & cpu choice, player & cpu score
+let playerChoice;
+let cpuChoice;
+let playerScore;
+let cpuScore;
+//Generate function for cpu choice
 function getComputerChoice() {
    //draw random number from 1-3, store in cpuChoice
-    let cpuChoice = Math.floor((Math.random()*3))+1
+    cpuChoice = Math.floor((Math.random()*3))+1;
     //If cpuChoice is 1, generate "rock"; if 2, generate "paper";
-    //if 3, generate scissors
+    //if 3, generate scissors; store result in cpuChoice
     if (cpuChoice === 1) {
-        console.log("rock");
+        cpuChoice = "rock";
     }
     else if (cpuChoice === 2) {
-        console.log("paper");
+        cpuChoice = "paper";
     }
     else if (cpuChoice == 3) {
-            console.log("scissors");
+            cpuChoice = "scissors";
         }
+    return cpuChoice;
     }
+//create function for player's choice
+function getPlayerChoice() {
+    //set while loop by creating keepGoing variable
+    let keepGoing = true;
+    while (keepGoing == true) {
+        //ask player for choice, store in variable
+        playerChoice = prompt("rock, paper, or scissors?");
+        //If user cancels, don't let them!
+        if (playerChoice === null) {
+            alert("not so fast, buddy")
+        }
+        //otherwise, test for valid input
+            //If valid, tell player their choice,
+            // set keepgoing to false to end the loop
+            //and store their choice in lowercase for 
+            // comparisons
+        else if (playerChoice.toLocaleLowerCase() === "rock" |
+        playerChoice.toLocaleLowerCase() === "paper" |
+        playerChoice.toLocaleLowerCase() === "scissors") {
+            keepGoing = false;
+            playerChoice = playerChoice.toLocaleLowerCase();
+        } 
+        //otherwise, tell player input is invalid, and repeat
+        else (alert("invalid input; try again!"))
+    }
+    return playerChoice;
+    }
+//Let's play a round
